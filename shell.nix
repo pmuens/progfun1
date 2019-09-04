b88dcaddbe5d7080c99a182e6aff071db3e0c1ee
Philipp Muens <raute1337@gmx.de>
2019-09-04 15:14:25 +0200

    Setup Nix

---
+++ shell.nix
@@ -0,0 +1,16 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  name = "dev";
+  env = buildEnv {
+    name = name;
+    paths = buildInputs;
+  };
+  buildInputs = [
+    jdk
+    jre
+    scala
+    maven
+    gradle
+    sbt
+  ];
+}
